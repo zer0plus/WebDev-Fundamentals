79874322e9d42e9b3b02bb5bd54edbb835e567b7
zer0plus <mitanshd108@gmail.com>
2021-02-07 10:09:00 -0500

    initial t2

---
+++ Tutorials/T2/problem_1.js
@@ -0,0 +1,88 @@
+document.getElementById("addBtn").addEventListener("click", addToList);
+document.getElementById("remBtn").addEventListener("click", removeFromList);
+document.getElementById("highlight").addEventListener("click", highlightItems);
+
+function addToList()
+{
+    let addElement = document.createElement("p");
+    let checkB = document.createElement("input");
+    let input = document.getElementById("task_input");
+    
+    if(input.value.length > 0 && input.value.trim().length>0)
+    {
+        checkB.type="checkbox";
+        checkB.name = "tick";
+        addElement.appendChild(checkB);
+
+        let node = document.createTextNode(input.value);
+        input.value = '';
+        addElement.appendChild(node);
+
+        let listAdd = document.getElementById("list");
+        listAdd.appendChild(addElement);
+    }
+
+    else
+    {
+        input.value='';
+        alert("Nothing Entered");
+    }
+
+    
+};
+
+function removeFromList()
+{
+    let listAdd = document.getElementById("list");
+    let para = listAdd.getElementsByTagName("p");
+    let checklist = listAdd.getElementsByTagName("input");
+    
+    let i=0;
+    for(i; i < para.length ;i++)
+    {
+
+        if(checklist[i].checked)
+        {
+            listAdd.removeChild(para[i]);
+            i--;
+        }
+    }
+
+    
+
+    //let addElement = document.createElement("p");
+    //let node = document.createTextNode(checklist.length);
+    //addElement.appendChild(node);
+    //listAdd.appendChild(addElement);
+
+}
+
+function highlightItems()
+{
+    let listAdd = document.getElementById("list");
+    let para = listAdd.getElementsByTagName("p");
+    let checklist = listAdd.getElementsByTagName("input");
+    
+    let i=0;
+    for(i; i < para.length ;i++)
+    {
+
+        if(checklist[i].checked)
+        {
+            if(para[i].style.color=="white")
+            {
+                para[i].style.color="black";
+                para[i].style.backgroundColor = "white";
+                
+                checklist[i].checked = false;
+            }
+            else
+            {
+                para[i].style.color="white";
+                para[i].style.backgroundColor = "grey";
+                checklist[i].checked = false;
+            }
+        }
+    }
+
+}
