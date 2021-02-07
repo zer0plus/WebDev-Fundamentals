d4e4c84d04ea84815a0628a4402569b05dd64d01
zer0plus <mitanshd108@gmail.com>
2021-02-07 10:36:39 -0500

    e

--- Tutorials/T2/problem_1.js
+++ Tutorials/T2/problem_1.js
@@ -1,15 +1,14 @@
 document.getElementById("addBtn").addEventListener("click", addToList);
 document.getElementById("remBtn").addEventListener("click", removeFromList);
 document.getElementById("highlight").addEventListener("click", highlightItems);
+document.getElementById("sortBtn").addEventListener("click", sortItems);
 
-function addToList()
-{
+function addToList(){
     let addElement = document.createElement("p");
     let checkB = document.createElement("input");
     let input = document.getElementById("task_input");
     
-    if(input.value.length > 0 && input.value.trim().length>0)
-    {
+    if(input.value.length > 0 && input.value.trim().length>0){
         checkB.type="checkbox";
         checkB.name = "tick";
         addElement.appendChild(checkB);
@@ -21,68 +20,52 @@ function addToList()
         let listAdd = document.getElementById("list");
         listAdd.appendChild(addElement);
     }
-
-    else
-    {
+    else{
         input.value='';
         alert("Nothing Entered");
     }
-
-    
 };
 
-function removeFromList()
-{
+function removeFromList(){
     let listAdd = document.getElementById("list");
     let para = listAdd.getElementsByTagName("p");
     let checklist = listAdd.getElementsByTagName("input");
     
     let i=0;
-    for(i; i < para.length ;i++)
-    {
-
-        if(checklist[i].checked)
-        {
+    for(i; i < para.length ;i++){
+        if(checklist[i].checked){
             listAdd.removeChild(para[i]);
             i--;
         }
     }
-
-    
-
     //let addElement = document.createElement("p");
     //let node = document.createTextNode(checklist.length);
     //addElement.appendChild(node);
     //listAdd.appendChild(addElement);
-
 }
 
-function highlightItems()
-{
+function highlightItems(){
     let listAdd = document.getElementById("list");
     let para = listAdd.getElementsByTagName("p");
     let checklist = listAdd.getElementsByTagName("input");
     
     let i=0;
-    for(i; i < para.length ;i++)
-    {
-
-        if(checklist[i].checked)
-        {
-            if(para[i].style.color=="white")
-            {
+    for(i; i < para.length ;i++){
+        if(checklist[i].checked){
+            if(para[i].style.color=="white"){
                 para[i].style.color="black";
                 para[i].style.backgroundColor = "white";
-                
-                checklist[i].checked = false;
+                // checklist[i].checked = false;
             }
-            else
-            {
+            else{
                 para[i].style.color="white";
                 para[i].style.backgroundColor = "grey";
-                checklist[i].checked = false;
+                // checklist[i].checked = false;
             }
         }
     }
+}
 
+function sortItems(){
+    
 }
