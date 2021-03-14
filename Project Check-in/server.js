53f1c3610088376ee6a1a77aeae9a08e8d004d87
zer0plus <mitanshd108@gmail.com>
2021-03-14 12:53:10 -0400

    ini

--- Project Check-in/server.js
+++ Project Check-in/server.js
@@ -1,2 +1,24 @@
 const express = require('express');
 let app = express();
+app.use(express.static("public"));
+
+app.set("view engine","pug");
+
+app.get("/people/exampleperson",function(req,res){
+	res.format({
+		"text/html":() =>{res.render("examplePerson");}
+	});
+
+	res.status(200);
+})
+
+app.get("/movies/exampleMovie",function(req,res){
+	res.format({
+		"text/html":() =>{res.render("exampleMovie");}
+	});
+
+	res.status(200);
+})
+
+app.listen(3000);
+console.log("Server listening at http://localhost:3000");
